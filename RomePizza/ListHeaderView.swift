16edfe2e2efa276cf3d53159181994d7243fc128
davalcato <davalcato@yahoo.com>
2019-12-06 17:39:03 -0800

    made a new file and named it ListHeaderView

---
+++ RomePizza/ListHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  ListHeaderView.swift
+//  RomePizza
+//
+//  Created by Daval Cato on 12/6/19.
+//  Copyright © 2019 Daval Cato. All rights reserved.
+//
+
+import SwiftUI
+
+struct ListHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListHeaderView()
+    }
+}
