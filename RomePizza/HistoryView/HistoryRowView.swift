b1fdf29e19b92c001089e803c0999384a24808e1
davalcato <davalcato@yahoo.com>
2019-12-06 17:10:49 -0800

    added the HistoryRowView file

---
+++ RomePizza/HistoryView/HistoryRowView.swift
@@ -0,0 +1,21 @@
+//
+//  HistoryRowView.swift
+//  RomePizza
+//
+//  Created by Daval Cato on 12/6/19.
+//  Copyright © 2019 Daval Cato. All rights reserved.
+//
+
+import SwiftUI
+
+struct HistoryRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HistoryRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryRowView()
+    }
+}
