18c02e9ed5a64fbab1c10ac8242a0005368361e9
davalcato <davalcato@yahoo.com>
2019-12-06 17:20:57 -0800

    added ContentHeaderView() PageTitleView(title: "Pizza History") and HistoryListView() to the HistoryView file

--- RomePizza/HistoryView/HistoryView.swift
+++ RomePizza/HistoryView/HistoryView.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct HistoryView: View {
     var body: some View {
         VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+            ContentHeaderView()
+            PageTitleView(title: "Pizza History")
+            HistoryListView()
         }
     }
 }
