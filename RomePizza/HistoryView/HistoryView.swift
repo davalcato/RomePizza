86e17952d80020f147f69e5a0f6fea5cea936270
davalcato <davalcato@yahoo.com>
2019-12-06 17:17:28 -0800

    added the HistoryView file and embedded a VStack

---
+++ RomePizza/HistoryView/HistoryView.swift
@@ -0,0 +1,23 @@
+//
+//  HistoryView.swift
+//  RomePizza
+//
+//  Created by Daval Cato on 12/6/19.
+//  Copyright © 2019 Daval Cato. All rights reserved.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryView()
+    }
+}
