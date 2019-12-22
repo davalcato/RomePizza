ef92a5607c24522f753062561fb45da3f29118d3
davalcato <davalcato@yahoo.com>
2019-12-22 11:44:01 -0800

    added the  @Binding var orderModel:OrderModel to the NavigationLink in the MenuListView

--- RomePizza/MenuView/MenuListView.swift
+++ RomePizza/MenuView/MenuListView.swift
@@ -9,15 +9,17 @@
 import SwiftUI
 
 struct MenuListView: View {
+    @Binding var orderModel:OrderModel
     var menuList = MenuModel().menu
     var body: some View {
         VStack {
-            ListHeaderView(text: "Menu")
+            ListHeaderView(text:"Menu")
             NavigationView{
                 List(menuList) { item in
-                    NavigationLink(destination:MenuDetailView(menuItem: item)){
+                    
+                    NavigationLink(destination:MenuDetailView(orderModel: self.$orderModel, menuItem: item)){
                         MenuRowView(menuItem: item)
-                            .listRowInsets(EdgeInsets())
+                        .listRowInsets(EdgeInsets())
                     }
                 }
             .navigationBarTitle("Pizza Order")
@@ -28,8 +30,7 @@ struct MenuListView: View {
 
 struct MenuListView_Previews: PreviewProvider {
     static var previews: some View {
-        MenuListView()
-        
+        MenuListView(orderModel:.constant(OrderModel()))
     }
 }
 
--- RomePizza/OrderView/OrderListView.swift
+++ RomePizza/OrderView/OrderListView.swift
@@ -9,12 +9,13 @@
 import SwiftUI
 
 struct OrderListView: View {
+    var orderModel:OrderModel
     var body: some View {
         VStack {
             Spacer()
             ListHeaderView(text: "Your Order")
-            List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-                OrderRowView()
+            List(orderModel.orders){ item in
+                OrderRowView(orderItem:item)
             }
         }
     }
@@ -22,7 +23,7 @@ struct OrderListView: View {
 
 struct OrderListView_Previews: PreviewProvider {
     static var previews: some View {
-        OrderListView()
+        OrderListView(orderModel:OrderModel())
     }
 }
 
