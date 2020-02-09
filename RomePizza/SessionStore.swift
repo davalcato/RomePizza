47e0fb7f537a8747b864f6cde7f009e290865e73
davalcato <davalcato@yahoo.com>
2020-02-09 13:24:01 -0800

    added the SessionStore file. Then added the ObservableObject to the class along with var didChange for the email and uid.

---
+++ RomePizza/SessionStore.swift
@@ -0,0 +1,29 @@
+//
+//  SessionStore.swift
+//  RomePizza
+//
+//  Created by Daval Cato on 2/9/20.
+//  Copyright © 2020 Daval Cato. All rights reserved.
+//
+
+import SwiftUI
+import Firebase
+import Combine
+
+
+class SessionStore: ObservableObject {
+    var didChange = PassthroughSubject<SessionStore, Never>()
+    @Published var session: User? {didSet {self.didChange.send(self) }}
+    var handle: AuthStateDidChangeListenerHandle?
+  
+}
+
+struct User {
+    var uid: String
+    var email: String?
+    
+    init(uid: String, email: String?) {
+        self.uid = uid
+        self.email = email
+    }
+}
