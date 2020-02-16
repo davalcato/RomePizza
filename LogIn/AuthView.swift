a1b62b244ddc2ca815d2bf3f70e501484d43ec57
davalcato <davalcato@yahoo.com>
2020-02-16 11:20:42 -0800

    added the foregroundColor(.gray) for the sign in to continue

--- LogIn/AuthView.swift
+++ LogIn/AuthView.swift
@@ -33,13 +33,13 @@ struct SignInView: View {
             
             Text("Sign in to continue")
                 .font(.system(size: 16, weight: .medium))
-//            .foregroundColor(Color("Black"))
+                .foregroundColor(.gray)
   
             
             VStack(spacing: 18) {
                 TextField("Email address", text: $email)
                     .font(.system(size: 14))
-                .padding(12)
+                    .padding(12)
                     .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color("bg1"), lineWidth: 1))
                 
                 SecureField("Password", text: $password)
@@ -57,9 +57,6 @@ struct SignInView: View {
                     .foregroundColor(.white)
                     .font(.system(size: 14, weight: .bold))
                     .background(LinearGradient(gradient: Gradient(colors: [Color("bg1"), Color("bg2")]), startPoint: .leading, endPoint: .trailing))
-                
-//                    .background(LinearGradient(gradient: Gradient(colors: [Color("bgi"), Color("bg2")]), startPoint: .leading, endPoint: .trailing))
-                
             }
         }
     }
